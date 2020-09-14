fc0b9df01637b5156fe19882570eda29a118f234
Пользователь <mehrana.mahmudova@gmail.com>
2020-09-14 04:07:46 +0300

    dobaila homework9

---
+++ NASLEDOVANIYE 2/NASLEDOVANIYE 2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+namespace NASLEDOVANIYE_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IndividualAccount count = new IndividualAccount();
+            count.Sum = 1000;
+            count.Open_data = new DateTime(2000, 12, 2);
+            count.Account_number = 54545454;
+            count.Sum1();
+            count.OpenData();
+            count.Percent(15);
+            count.Cashout();
+        }
+    }
+
+
+    class Count
+    {
+        public int Sum
+        {
+            get; set;
+        }
+        public DateTime Open_data
+        {
+            get; set;
+        }
+        public int Account_number
+        {
+            get; set;
+        }
+
+        public void Sum1()
+        {
+            Console.WriteLine("Summa na balanse: " + Sum);
+        }
+
+        public void OpenData()
+        {
+            Console.WriteLine($"Data otkritiya balansa: " + Open_data);
+        }
+
+
+    }
+
+
+    class IndividualAccount : Count
+    {
+        public string Count_Type { get; set; }
+
+        public void Percent(double percent)
+        {
+            double a = Sum * percent / 100;
+            double b = Sum + a;
+            Console.WriteLine("Summa nachislennix procentov: " + a);
+            Console.WriteLine("Obshaya obnovlennaya summa: " + b);
+        }
+
+        public void Cashout()
+        {
+            double cashout = 250;
+            if (cashout <= Sum)
+            {
+                double ostatok = Sum - cashout;
+                Console.WriteLine("Udachnje snatie so scheta! Ostalos na shetu: " + ostatok);
+            }
+        }
+
+    }
+
+    class YurAccount : Count
+    {
+        public void Percent(double percent)
+        {
+            double a = Sum * percent / 100;
+            double b = Sum + a;
+            Console.WriteLine("Summa nachislennix procentov: " + a);
+            Console.WriteLine("Obshaya obnovlennaya summa: " + b);
+        }
+    }
+
+
+}
+
+
+
+
+
---
+++ NASLEDOVANIYE/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace NASLEDOVANIYE
+{
+    class Program
+        {
+            static void Main(string[] args)
+            {
+                while (true)
+                {
+
+                    Console.WriteLine("Write surname");
+                    string b = Console.ReadLine();
+                    Console.WriteLine("Write course of study");
+                    int c = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Write grade number book");
+                    int d = Convert.ToInt32(Console.ReadLine());
+                    Student a = new Student(b, c, d);
+
+
+                    Console.WriteLine("Write name");
+                    string e = Console.ReadLine();
+                    Aspirant j = new Aspirant(e, b, c, d);
+                    j.Print();
+                    bool name = true;
+                    foreach (char i in e)
+                    {
+                        if (i >= '0' && i <= '9')
+                        {
+                            name = false;
+                            Console.WriteLine("input letters");
+                            break;
+                        }
+                    }
+                    if (name == false)
+                    {
+                        continue;
+                    }
+                    break;
+                }
+            }
+
+            /*Console.WriteLine("Write surname");
+            string f = Console.ReadLine();
+            Console.WriteLine("Write course of study");
+            int g = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Write grade number book");
+            int h = Convert.ToInt32(Console.ReadLine());*/
+        }
+}
+    class Student
+
+    {
+        public string Surname
+
+        {
+            get;
+
+            set;
+
+
+        }
+        public int Course_of_study
+        {
+
+            get;
+
+            set;
+        }
+        public int Grade_book_number { get; set; }
+
+        public Student(string surname, int course_of_study, int grade_book_number)
+        {
+            Surname = surname;
+            Course_of_study = course_of_study;
+            Grade_book_number = grade_book_number;
+        }
+
+    }
+    class Aspirant : Student
+    {
+        public string Name { get; set; }
+
+        public Aspirant(string name, string surname, int course_of_study, int grade_book_number)
+            : base(surname, course_of_study, grade_book_number)
+        {
+            Name = name;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"name {Name} ,surname {Surname} ,course of study {Course_of_study} ,grade book number {Grade_book_number}");
+        }
+    }
+
+ 
